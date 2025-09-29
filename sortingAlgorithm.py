8a0077054003c6cba23e529c5c8b9eacc4b467af
Baum1234567 <128495936+Baum1234567@users.noreply.github.com>
2025-09-29 20:38:55 +0200

    Add files via upload

--- sortingAlgorithm.py
+++ sortingAlgorithm.py
@@ -2,17 +2,18 @@
 import sys                                  # import sys to use sys functions
 import random                               # import random to use random functions
 import pygame as py                         # import pygame to use pygame functions
+import time                                 # import time to use time functions
 
 #---------main function---------#
 def main(n):
 
     #---------create a shuffled list---------#
-    createShuffledList()
+    numbers = createShuffledList()
 
     #---------init pygame---------#
     py.init()                                               # init pygame
-    width = 700                                             # set screen width and height
-    height = 400
+    width = 1280                                             # set screen width and height
+    height = 720
     screen = py.display.set_mode((width, height))           # create screen
     py.display.set_caption("Sorting Algorithm 1")           # set caption from the screen
     running = True
@@ -22,15 +23,26 @@ def main(n):
         for event in py.event.get():                        # and will update everything
             if event.type == py.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))                              # background is black
+
+        drawRects(screen, numbers, width, height)           # draw bars
+
+
         py.display.update()
+        time.sleep(0.1)
     py.quit()
 
 #----------function to draw rects-------------#
-def drawRects(lst):
+def drawRects(screen, lst, width, height):
+    bar_width = width // len(lst)                           # every bar has the same width
+    max_value = max(lst)                                    # highest value in the list
+    green = (0, 255, 0)
 
-    for nums in lst:
-        py.rect
+    for i, val in enumerate(lst):
+        bar_height = int((val / max_value) * height)
+        x = i * bar_width
+        y = height - bar_height
+        py.draw.rect(screen, green, (x, y, bar_width, bar_height))
 
 #----------function to create a shuffled list-------------#
 def createShuffledList():
@@ -44,6 +56,7 @@ def createShuffledList():
     print(numbersToSort)
     shuffledNumbers = shuffel(numbersToSort)
     print(shuffledNumbers)
+    return shuffledNumbers
 
 #----------function to shuffle a list-------------#
 def shuffel(lst):
